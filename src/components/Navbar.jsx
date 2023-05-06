9d6fc646296812a3ae7db70da491cef84942ca5f
Cüneyt Kaya <53535761+kayacuneyd@users.noreply.github.com>
2023-05-06 20:38:13 +0000

    centered navi on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      <div className="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+      <div className="w3-center w3-top w3-hide-large w3-hide-medium" id="myNavbar">
         <div className="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
           <a
             href="#home"
